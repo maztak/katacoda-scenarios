65ab7bcedb8eee3acbfbe79b067be1fe8b1d5d98
Takuya Matsuda <takuya108817@gmail.com>
2020-07-03 09:31:39 +0900

    change scenario title and separate three

---
+++ create-line-official-account/intro_code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# install node.js using n
+sudo npm install -g n
+# change node v12.15.0
+sudo n 12.16.1
+# clone hands-on code
+git clone https://github.com/sumihiro3/line-bot-hands-on-vol1.git example
+# install dependent modules
+cd example
+npm install
+clear
+# Done!!
+echo "ソースコードのインストールが完了しました！"
